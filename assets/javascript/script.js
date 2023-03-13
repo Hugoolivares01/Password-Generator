bab88fb532a8e387a4ae460a89434ba990ba629d
Juice <hugoolivares01@gmail.com>
2023-03-13 13:48:27 -0700

    added start

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,24 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var generateBtn = document.querySelector("#generate");
+//  stores the generate id  into the var generateBtn
 function writePassword() {
   var password = generatePassword();
+  // whatever the  Generatepassword function creates it will store it in the var password which will be used in 
+  // line 15
+
   var passwordText = document.querySelector("#password");
+  // this targets the password id and stores it in the var passwordText
+
 
   passwordText.value = password;
+  // this then targets the value of the passwordText in this case "your secure password" and replaces it with whatever the 
+  // var password is equal too
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+
+  return "wazz up"
+}
