bad6431b615958a7160e7e2ba3c410a4514c6838
Juice <hugoolivares01@gmail.com>
2023-03-13 14:23:20 -0700

    check point 1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -15,10 +15,29 @@ function writePassword() {
   // var password is equal too
 
 }
+var characterLength = 8;
+var choiceArr = []
+var specialchararr = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "(", ")",]
+var lowercasearr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+  "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numberArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
 
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 
-  return "wazz up"
+}
+function getPrompts() {
+  characterLength = parseInt(prompt( "ranging from (8 - 128) how many characters do you want for your password?"));
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+    alert("character length must be from (8-128). ");
+    return false;
+  }
+  if (confirm("Do you want any lowercase letters in your pasword? "))
+  // choiceArr = choiceArr.concat(lowercasearr);
+
+  return password;
 }
